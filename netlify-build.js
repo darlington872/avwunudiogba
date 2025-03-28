01100c2c88157750d98adea8caa649f90779b71e
Keno Darlington <40947710-him28@users.noreply.replit.com>
2025-03-28 15:27:47 +0000

    Fix netlify-build.js to use ES modules syntax

--- netlify-build.js
+++ netlify-build.js
@@ -1,5 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get current directory in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Create netlify functions directory if it doesn't exist
 const functionsDir = path.join(__dirname, 'netlify', 'functions');
